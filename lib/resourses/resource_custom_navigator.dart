a793a2a6762f8e15c4e548e0c96e1bb96d936934
macbookpro <1>
2019-08-18 11:45:50 -0300

    - Criada classe que contem as animacoes customizadas

--- lib/resourses/resource_custom_navigator.dart
+++ lib/resourses/resource_custom_navigator.dart
@@ -1,29 +1,15 @@
 import 'package:flutter/material.dart';
-
+import 'custom_transition_animations.dart';
 class CustomNavigator{
-  static Widget slideTransition(Animation<double> animation, Animation<double> secondaryAnimation, Widget child) {
-    return SlideTransition(
-      position: new Tween<Offset>(
-        begin: const Offset(1.0, 0.0),
-        end: Offset.zero,
-      ).animate(animation),
-      child: new SlideTransition(
-        position: new Tween<Offset>(
-          begin: Offset.zero,
-          end: const Offset(1.0, 0.0),
-        ).animate(secondaryAnimation),
-        child: child,
-      ),
-    );
-  }
 
   static push(BuildContext context, newRoute) {
     final pageRoute = PageRouteBuilder(
       pageBuilder: (BuildContext context, Animation<double> animation, Animation<double> secondaryAnimation) {
         return newRoute;
       },
+      transitionDuration: Duration(seconds: 2),
       transitionsBuilder: (BuildContext context, Animation<double> animation, Animation<double> secondaryAnimation, Widget child) {
-        return slideTransition(animation, secondaryAnimation, child);
+        return CustomTransitionAnimations.scaleTransition(animation, secondaryAnimation, child);
       },
     );
 
@@ -50,7 +36,7 @@ class CustomNavigator{
         return newRoute;
       },
       transitionsBuilder: (BuildContext context, Animation<double> animation, Animation<double> secondaryAnimation, Widget child) {
-        return slideTransition(animation, secondaryAnimation, child);
+        return CustomTransitionAnimations.slideTransitionLeft(animation, secondaryAnimation, child);
       },
     );
 
